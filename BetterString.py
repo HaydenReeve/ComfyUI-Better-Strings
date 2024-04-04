d1eaf01716105fc3251041914d161fc0b87a2fe4
Hayden Reeve <haydenreeve@hotmail.com>
2024-04-05 01:27:02 +0800

    Refactor BetterString class into separate module
    The class BetterString has been relocated from __init__.py and moved to
    a newly created separate module BetterString.py. This reorganization
    improves modularity and makes the code easier to maintain.
    
    Additionally, a new optional stacking string has been added to allow you
     to sequence several nodes in a row and concatenate their results!
    
    Neat!

---
+++ BetterString.py
@@ -0,0 +1,26 @@
+class BetterString:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "optional": {
+                "chain": ("STRING", {"multiline": False}),
+            },
+            "required": {
+                "string": ("STRING", {"multiline": True}),
+            },
+        }
+
+    RETURN_TYPES = ("STRING",)
+    CATEGORY = "Better Things 💡"
+
+    def action(self, string, chain="", ):
+        if not chain.strip():
+            return (string,)
+
+        chain = chain.rstrip()
+        if chain[-1] == ",":
+            chain += ", "
+
+        return (chain + string,)
+
+    FUNCTION = action.__name__
--- __init__.py
+++ __init__.py
@@ -1,29 +1,13 @@
-class BetterString:
+from .BetterString import *
 
-    def __init__(self):
-        pass
-
-    @classmethod
-    def INPUT_TYPES(s):
-        return {
-            "required": {
-                "string": ("STRING", {"multiline": True}),
-            },
-        }
-
-    FUNCTION = "event"
-    RETURN_TYPES = ("STRING",)
-
-    CATEGORY = "Better Things 💡"
-
-    def event(self, string):
-        return (string, )
-
+# from .BetterPath import *
 
 NODE_CLASS_MAPPINGS = {
-    "BetterString": BetterString
+    "BetterString": BetterString,
+    # "BetterPath": BetterPath,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "BetterString": "Better String"
+    "BetterString": "Better Multiline String",
+    # "BetterPath": "Better Path Selector",
 }
