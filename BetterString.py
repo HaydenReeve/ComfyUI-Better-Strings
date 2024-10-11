27dcb78e753adf85d0c2ceca35e5150aa4bae18e
Hayden Reeve <39004735+HaydenReeve@users.noreply.github.com>
2024-10-11 22:54:15 +0800

    Added the ability to chain together multiple multiline string nodes (#4)
    ![image](https://github.com/user-attachments/assets/fb2841ec-f2ef-4802-acae-eb76697391be)
    
    Nodes can now chain together, which lets you build lovely, natural
    sentence fragments.
    
    There's nothing particularly fancy or special going on here, it doesn't
    handle tokenisation in any particular way, it simply lets you structure
    your inputs in a nicer format if you feel so inclined. :)

---
+++ BetterString.py
@@ -0,0 +1,28 @@
+class BetterString:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "optional": {
+                "chain": ("STRING", {"multiline": False, "forceInput": True, "defaultInput": True}),
+            },
+            "required": {
+                "string": ("STRING", {"multiline": True}),
+            },
+        }
+
+    RETURN_TYPES = ("STRING",)
+    CATEGORY = "Better Things 💡"
+
+    def action(self, string, chain="", ):
+        if not chain.strip():
+            return (string,)
+
+        chain = chain.rstrip()
+        if chain[-1] != ",":
+            chain += ","
+
+        chain += " "
+
+        return (chain + string,)
+
+    FUNCTION = action.__name__
