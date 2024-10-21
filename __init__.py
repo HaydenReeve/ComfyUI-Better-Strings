053e0d9d3f54275833a82cc4fc508d896e81939d
Hayden Reeve <haydenreeve@hotmail.com>
2024-10-21 20:24:05 +0800

    Removed references to experimental nodes until further testing is done

--- __init__.py
+++ __init__.py
@@ -1,13 +1,9 @@
 from .BetterString import *
 
-# from .BetterPath import *
-
 NODE_CLASS_MAPPINGS = {
     "BetterString": BetterString,
-    # "BetterPath": BetterPath,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "BetterString": "Better Multiline String",
-    # "BetterPath": "Better Path Selector",
+    "BetterString": "Better Multiline String 💡",
 }
